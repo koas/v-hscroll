03edea5d6c97c9a4a28247398dc22de0ffadcdd7
Koas <koas@users.noreply.github.com>
2022-02-22 06:37:45 +0100

    Added value to specify a selector where the scroll will be applied.

--- v-hscroll.js
+++ v-hscroll.js
@@ -1,35 +1,56 @@
 import Vue from 'vue';
 
-function scrollHorizontally(e) 
+function scrollHorizontally(e)
 {
-	e = window.event || e;
+  e = window.event || e;
 
-	let element = e.target;
-	while (!element.getAttribute("v-hscroll-main"))
-		element = element.parentNode;
+  let element = e.target;
+  while (!element.getAttribute("v-hscroll-main"))
+    element = element.parentNode;
 
-	let delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-	element.scrollLeft -= (delta * 40);
+  let delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+  element.scrollLeft -= (delta * 40);
 
-	if ((delta > 0 && element.scrollLeft > 0) || 
-			(delta < 0 && element.offsetWidth + element.scrollLeft < element.scrollWidth))
-	{
-		e.preventDefault();
-	}
+  if ((delta > 0 && element.scrollLeft > 0) ||
+    (delta < 0 && element.offsetWidth + element.scrollLeft < element.scrollWidth))
+  {
+    e.preventDefault();
+  }
 };
 
-const HScroll = {
-	
-	bind(el, bindings, vn)
-	{
-		el.setAttribute("v-hscroll-main", "1");
-		el.addEventListener('wheel', scrollHorizontally, false);
-	},
+function waitForSelector(el, binding, vn)
+{
+  const newEl = el.querySelector(binding.value);
 
-	unbind(el, bindings, vn)
-	{
-		el.removeEventListener('wheel', scrollHorizontally, false);
-	}
+  if (newEl)
+    attachToElement(newEl);
+  else setTimeout(() => waitForSelector(el, binding, vn), 100);
+
+}
+
+function attachToElement(el)
+{
+  el.setAttribute("v-hscroll-main", "1");
+  el.addEventListener('wheel', scrollHorizontally, false);
+}
+
+const HScroll = {
+
+  bind(el, binding, vn)
+  {
+    if (binding.value)
+    {
+      waitForSelector(el, binding, vn);
+      return;
+    }
+
+    attachToElement(el);
+  },
+
+  unbind(el, binding, vn)
+  {
+    el.removeEventListener('wheel', scrollHorizontally, false);
+  }
 };
 
 Vue.directive('hscroll', HScroll);
